fbe21bb1edd0d5332b563304c905990cddd58440
Tomas Yanez <tomas.yanez86@gmail.com>
2024-11-15 15:46:38 +0000

    addeed function to retrieve workspace from azure

---
+++ src/utils/azureml_functions.py
@@ -0,0 +1,17 @@
+from azureml.core import Workspace
+from azureml.core.authentication import InteractiveLoginAuthentication
+
+
+def get_ws(stage="ml"):
+    stages = {"dev", "train"}
+    if stage not in stages:
+        raise ValueError("Invalid stage for workspace: got %s, should be from %s" %(stage, stages))
+
+    if stage in {"dev", "train"}:
+        print("Logging in as user")
+        credential = InteractiveLoginAuthentication()
+
+    config_path = ".cloud/.azure/config_{stage}.json".format(stage=stage.upper())
+    ws = Workspace.from_config(config_path, auth=credential)
+
+    return ws

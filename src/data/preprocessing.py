f134bc5a5b1a808ee087e9770e45e1eb7a0a2157
Tomas Yanez <tomas.yanez86@gmail.com>
2024-11-17 15:30:46 +0000

    preprocess data in local machine

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -5,12 +5,10 @@ from preprocessing_functions import preprocess_data
 
 if __name__ == "__main__":
     # load data
-    data = pd.read_csv("../../data/raw/pump_sensor.csv", index_col=0)
+    df = pd.read_csv("../../data/raw/pump_sensor.csv", index_col=0)
 
     # preprocess data
-    data = preprocess_data(data)
+    df = preprocess_data(df)
 
     # save data
-    data.to_csv("../data/processed/preprocessed_data.csv", index=False)
-
-    test
+    df.to_csv("../data/processed/preprocessed_data.csv", index=False)
--- src/data/preprocessing_functions.py
+++ src/data/preprocessing_functions.py
@@ -1,5 +1,11 @@
 # import libraries
 import pandas as pd
+from feature_engine.imputation import MeanMedianImputer
+from feature_engine.selection import (DropConstantFeatures,
+                                      DropDuplicateFeatures,
+                                      SmartCorrelatedSelection)
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
 
 
 # function to preprocess data
@@ -8,24 +14,46 @@ def preprocess_data(data):
     # convert the "timestamp" column to datetime format
     data["timestamp"] = pd.to_datetime(data["timestamp"])
 
-    # replace nan values with 0 only on columns starting with sensor_
-    data.loc[:, data.columns.str.startswith("sensor_")] = data.loc[
-        :, data.columns.str.startswith("sensor_")
-    ].fillna(0)
+    # drop columns with high missing values
+    data = data.drop(["sensor_00", "sensor_15", "sensor_50"], axis=1)
 
-    # Convert "machine_status" column to categorical type
-    data["machine_status"] = data["machine_status"].astype("category")
+    # select sensor columns
+    sensor_columns = [col for col in data.columns if col.startswith("sensor_")]
 
-    # Code the categories
-    data["machine_status_code"] = data["machine_status"].cat.codes
+    # fill missing values with the mean of the column
+    imputer = MeanMedianImputer(imputation_method="mean")
+    data[sensor_columns] = imputer.fit_transform(data[sensor_columns])
 
-    # create new time features
-    data["timestamp_year"] = data["timestamp"].dt.year
-    data["timestamp_month"] = data["timestamp"].dt.month
-    data["timestamp_day"] = data["timestamp"].dt.day
-    data["timestamp_hour"] = data["timestamp"].dt.hour
-    data["timestamp_minute"] = data["timestamp"].dt.minute
+    # Convert "machine_status" column to numerical categories
+    data["machine_status"] = data["machine_status"].map(
+        {"NORMAL": 0, "BROKEN": 1, "RECOVERING": 1, })
 
-    # encode categorical features
-    # data = pd.get_dummies(data)
-    return data
+    # Create a pipeline to drop constant and duplicate features and select correlated features
+    pipeline = Pipeline([
+        ("constant_features", DropConstantFeatures(tol=0.95)),
+        ("duplicated_features", DropDuplicateFeatures()),
+        ("correlated_features", SmartCorrelatedSelection()),
+    ])
+
+    # Apply the pipeline to the sensor columns
+    data_transformed = pipeline.fit_transform(data[sensor_columns])
+
+    sensor_columns = data_transformed.columns
+
+    # Scale features for sensor data
+    scaler = MinMaxScaler()
+    data_transformed = scaler.fit_transform(data_transformed)
+
+    data_transformed = pd.DataFrame(
+        data_transformed, columns=sensor_columns)
+
+    # Include the timestamp and machine_status columns
+    data_final = pd.concat(
+        [data[["timestamp", "machine_status"]], data_transformed], axis=1)
+
+    # move the machine_status column to the last column
+    data_final = data_final[[
+        col for col in data_final.columns if col != "machine_status"] + ["machine_status"]]
+
+    # Return the preprocessed data
+    return data_final

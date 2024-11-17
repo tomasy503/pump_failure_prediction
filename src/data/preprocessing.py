0bbbad155cd3dc26719a278ed564c98ecc2fa63d
Tomas Yanez <tomas.yanez86@gmail.com>
2024-11-17 10:21:38 +0000

    preprocessing step

---
+++ src/data/preprocessing.py
@@ -0,0 +1,17 @@
+# import libraries
+import numpy as np
+import pandas as pd
+
+from src.utils.preprocessing_functions import preprocess_data
+
+if __name__ == "__main__":
+    # load data
+    data = pd.read_csv("../../data/raw/pump_sensor.csv", index_col=0)
+
+    # preprocess data
+    data = preprocess_data(data)
+
+    # save data
+    data.to_csv("../data/processed/preprocessed_data.csv", index=False)
+
+    test
